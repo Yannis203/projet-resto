c3e1a9b0e7c547151842d6a244e5b2ada79ae4fb
Yannis <lubambayannis@gmail.com>
2025-06-14 20:08:28 +0100

    essais backend avec appscript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Partie front-end du projet (menu burger responsive)
 const menuHumberger=document.querySelector('.menu-icon');
 const menu =document.querySelector('.onglets');
 const liens=document.querySelectorAll('.onglets a');
@@ -12,3 +13,33 @@ liens.forEach(link =>{
 }
 
 )
+
+
+// // Partie backend du projet (avec appdcript )
+// document.getElementById("reservation-form").addEventListener("submit", function(event) {
+//   event.preventDefault(); // Empêche le rechargement de la page
+
+//   Récupération des données du formulaire
+//   const nom = document.getElementById("nom").value;
+//   const email = document.getElementById("email").value;
+//   const date = document.getElementById("date").value;
+//   const table = document.getElementById("table").value;
+
+//   // Envoi des données vers Apps Script (backend)
+//   fetch("https://script.google.com/macros/s/AKfycbz3xVWmh_QIxy-jc3oWJSmL7nLDoZFZW2rB2C5QQdFRzJiSj5QJetesp8J2NzztDe46/exec", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ nom, email, date, table }),
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//     document.getElementById("message").textContent = data.message;
+//   })
+//   .catch(error => {
+//     document.getElementById("message").textContent = "Erreur lors de l’envoi ";
+//     console.error("Erreur : ", error);
+//   });
+// });
+
