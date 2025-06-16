469983d24b67180046a220f8583fb2a925b1d86a
Yannis <lubambayannis@gmail.com>
2025-06-16 22:47:20 +0100

    modification section main responsive

--- script.js
+++ script.js
@@ -14,32 +14,3 @@ liens.forEach(link =>{
 
 )
 
-
-// // Partie backend du projet (avec appdcript )
-// document.getElementById("reservation-form").addEventListener("submit", function(event) {
-//   event.preventDefault(); // Empêche le rechargement de la page
-
-//   Récupération des données du formulaire
-//   const nom = document.getElementById("nom").value;
-//   const email = document.getElementById("email").value;
-//   const date = document.getElementById("date").value;
-//   const table = document.getElementById("table").value;
-
-//   // Envoi des données vers Apps Script (backend)
-//   fetch("https://script.google.com/macros/s/AKfycbz3xVWmh_QIxy-jc3oWJSmL7nLDoZFZW2rB2C5QQdFRzJiSj5QJetesp8J2NzztDe46/exec", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ nom, email, date, table }),
-//   })
-//   .then(response => response.json())
-//   .then(data => {
-//     document.getElementById("message").textContent = data.message;
-//   })
-//   .catch(error => {
-//     document.getElementById("message").textContent = "Erreur lors de l’envoi ";
-//     console.error("Erreur : ", error);
-//   });
-// });
-
